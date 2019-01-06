b42ffe4fb6537ae17359b00a91c5866acd5d8bed
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-01-06 23:39:04 +0200

    Create Calculator.java

---
+++ java/junit-explanation/src/main/java/cf/mindaugas/Calculator.java
@@ -0,0 +1,10 @@
+package cf.mindaugas;
+
+public class Calculator {
+    public static int add(int firstN, int secondN){
+        return firstN + secondN;
+    }
+    public static int subtract(int firstN, int secondN){
+        return firstN - secondN;
+    }
+}
