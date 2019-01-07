e61c30489221083671d3616e0edd16f87fd4b895
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-01-07 09:26:33 +0200

    Update mysql_seed.sql

--- java/pv/mysql_seed.sql
+++ java/pv/mysql_seed.sql
@@ -21,3 +21,31 @@ VALUES
 	('GD000623', 'MINDAUGAS', 'BERNATAVIČIUS', '2016-11-05 00:00:00', NULL, 'qwerty3', '1', b'1');
     
 SELECT * FROM pacientu_vizitacija.specialistai;
+
+
+
+CREATE TABLE `ligosistorija` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `ligos_istorijos_numeris` int(8) NOT NULL,
+  `paciento_asmens_kodas` bigint NOT NULL,
+  `anamnezė` varchar(60) DEFAULT NULL,
+  `diagnozė` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ligos_istorijos_numeris` (`ligos_istorijos_numeris`),
+  UNIQUE KEY `paciento_asmens_kodas` (`paciento_asmens_kodas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+INSERT INTO `pacientu_vizitacija`.`ligosistorija`
+	(`ligos_istorijos_numeris`, `paciento_asmens_kodas`, `anamnezė`, `diagnozė`)
+VALUES
+	(128,38524132244,"Skundžiasi bendru kvailumu","Senatvinė demencija"),
+    	(189,34513413224,"Skauda sažinę","Politvagio sindromas");
+
+
+SELECT * FROM pacientu_vizitacija.ligosistorija;
+
+
+
+
